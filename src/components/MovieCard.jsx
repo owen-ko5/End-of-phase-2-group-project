38f34fc978dd34036a47f7f25f6eae1ed3e5da0a
Tyra <tyra.mwai@student.moringaschool.com>
2025-04-22 11:43:43 +0300

    add fallback values for title,year and description

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,4 +3,10 @@ import { useNavigate } from 'react-router-dom';
 const MovieCard = ({ movie }) => {
     const navigate = useNavigate();
     const { id, title, name, poster_path, release_date, first_air_date, overview } = movie;
+    const movieTitle = title || name || "Unknown Title";
+    const movieYear = (release_date || first_air_date || "Unknown").split("-")[0];
+    const movieDesc = overview || "No description available";
+    const movieImage = poster_path
+     ? `https://image.tmdb.org/t/p/w500${poster_path}`
+     : "default-image.jpg";
 }
