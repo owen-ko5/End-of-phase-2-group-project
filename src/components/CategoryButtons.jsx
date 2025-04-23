989ae7a58d594b8488f17428708ba4048ce343ac
sarah <sarah.akelo@student.moringaschool.com>
2025-04-23 10:27:14 +0300

    category done

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const CategoryButtons = ({ onCategoryClick, onTrendingClick }) => (
   <div className="category-buttons">
+
     <button onClick={onTrendingClick}>Trending</button>
+
     <button onClick={() => onCategoryClick("movie")}>Movies</button>
+
     <button onClick={() => onCategoryClick("tv")}>Series</button>
+
     <button onClick={() => onCategoryClick("animation")}>Animation</button>
+    
   </div>
 );
 
